7a5951997ba5ed24d12f7c647908d669727cf8a8
Maggie Falkenberg <mf1306@yahoo.com>
2018-03-22 14:54:43 -0700

    created Sign Up

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SignUp.css";
+
+function SignUp(props) {
+	return (
+		<button onClick={props.onClick}>
+			Sign Up 
+		</button>	
+		);	
+}
+
+
+export default SignUp;
