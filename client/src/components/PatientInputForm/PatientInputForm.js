802ad0077359a4913c1d25c03b3c5cd22368a5cf
Stacy Areas <stacyareas@gmail.com>
2018-03-23 11:36:41 -0700

    add cols for form inputs in patientInputForm

--- client/src/components/PatientInputForm/PatientInputForm.js
+++ client/src/components/PatientInputForm/PatientInputForm.js
@@ -9,9 +9,11 @@ const PatientInputForm = props => (
 		    <form>
           <div className="row">
             <div className="col">
+            <label for="firstName">First Name</label>
               <input type="text" className="form-control" id="firstName">
             </div>
             <div className="col">
+              <label for="lastName">Last Name</label>
               <input type="text" className="form-control" id="lastName">
             </div>
           </div>
@@ -36,9 +38,56 @@ const PatientInputForm = props => (
               <label for="state">State</label>
               <select id="state" className="form-control">
                 <option selected>Choose...</option>
-                <option>California</option>
-                <option>Washington</option>
-                <option>Oregon</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="CA">California</option>
+                <option value="CO">Colorado</option>
+                <option value="CT">Connecticut</option>
+                <option value="DE">Delaware</option>
+                <option value="DC">District Of Columbia</option>
+                <option value="FL">Florida</option>
+                <option value="GA">Georgia</option>
+                <option value="HI">Hawaii</option>
+                <option value="ID">Idaho</option>
+                <option value="IL">Illinois</option>
+                <option value="IN">Indiana</option>
+                <option value="IA">Iowa</option>
+                <option value="KS">Kansas</option>
+                <option value="KY">Kentucky</option>
+                <option value="LA">Louisiana</option>
+                <option value="ME">Maine</option>
+                <option value="MD">Maryland</option>
+                <option value="MA">Massachusetts</option>
+                <option value="MI">Michigan</option>
+                <option value="MN">Minnesota</option>
+                <option value="MS">Mississippi</option>
+                <option value="MO">Missouri</option>
+                <option value="MT">Montana</option>
+                <option value="NE">Nebraska</option>
+                <option value="NV">Nevada</option>
+                <option value="NH">New Hampshire</option>
+                <option value="NJ">New Jersey</option>
+                <option value="NM">New Mexico</option>
+                <option value="NY">New York</option>
+                <option value="NC">North Carolina</option>
+                <option value="ND">North Dakota</option>
+                <option value="OH">Ohio</option>
+                <option value="OK">Oklahoma</option>
+                <option value="OR">Oregon</option>
+                <option value="PA">Pennsylvania</option>
+                <option value="RI">Rhode Island</option>
+                <option value="SC">South Carolina</option>
+                <option value="SD">South Dakota</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
               </select>
             </div>
 
@@ -48,19 +97,37 @@ const PatientInputForm = props => (
             </div>
           </div>
 
-    	    <div className="form-group">
-   		      <label for="primaryCare">Primary Care Physician</label>
-    	   	  <input type="text" className="form-control" id="primaryCareName" placeholder="Physician Name">
-    		    <input type="text" className="form-control" id="primaryCareNumb" placeholder="Physician's Phone Number">
-    	    </div>
+          <div className="form-row">
+      	    <div className="form-group col-md-6">
+     		      <label for="primaryCare">Primary Care Physician</label>
+      	   	  <input type="text" className="form-control" id="primaryCareName" placeholder="Physician Name">
+            </div>
+            
+            <div className="form-group col-md-6">  
+      		    <input type="text" className="form-control" id="primaryCareNumb" placeholder="Physician's Phone Number">
+      	    </div>
+          </div>  
 
-    	    <div className="form-group">
-   			    <label for="emergencyContact">Emergency Contact</label>
-    		    <input type="text" className="form-control" id="contactFirstName" placeholder="First Name">
-    		    <input type="text" className="form-control" id="contactLastNAme" placeholder="Last Name">
-    		    <input type="text" className="form-control" id="contactRelationship" placeholder="Relationship">
-    		    <input type="text" className="form-control" id="contactNumb" placeholder="Phone Number">
-    	    </div>
+          <div className="form-row"> 
+      	    <div className="form-group col-md-6">
+     			    <label for="emergencyContact">Emergency Contact</label>
+      		    <input type="text" className="form-control" id="contactFirstName" placeholder="First Name">
+            </div>
+              
+            <div className="form-group col-md-6">  
+      		    <input type="text" className="form-control" id="contactLastNAme" placeholder="Last Name">
+            </div>
+          </div>
+
+          <div className="form-row">
+            <div className="form-group col-md-6">     
+      		    <input type="text" className="form-control" id="contactRelationship" placeholder="Relationship">
+            </div>
+            
+            <div className="form-group col-md-6">  
+      		    <input type="text" className="form-control" id="contactNumb" placeholder="Phone Number">
+      	    </div>
+          </div>  
 
     	    <div className="form-group">
    		    	<label for="language">Preferred Language</label>
