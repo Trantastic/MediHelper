f080eb87ecd9e059b3a1cdcb44d98d13d98080d8
Yvette <yvettetran@gmail.com>
2018-03-23 13:38:09 -0700

    starting code for subcomponent

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import './PatientDropDown';
 // import calendar component
-// import patient drop down component
+// import API methods
 
 class Dashboard extends Component {
 	/*Psuedo-code
@@ -12,18 +13,43 @@ Hierarchy:
 				->PatientProfile || PatientInputForm
 
 	*/
+
+	state = {
+		patient: [],
+		calendar: []
+	};
+
 	componentDidMount(){
 		// API calls to DB to get all patients and calendar events
 		this.getPatients();
 		this.getCalendar();
 	}
 
+	// Retrieves all patients associated with caretaker and 
+	// creates a button for each of them in the drop drop menu
+	/*
+	getPatients = () => {
+		API.getPatients()
+			.then(res =>
+				this.setState({patient: res.data.name})
+			)
+			.catch(error => console.log(error));
+	};
+
+	*/
+
 	render(){
 		return(
 			<div>
 				<Calendar />
 				<PatientDropDown 
-					getPatients: {this.getPatients}
+					// getPatients: {this.getPatients}
+					{this.state.patient.map(patient => (
+						<DropdownListItem 
+						patientName: {patient.name}
+
+						/>
+					))}		
 				/>
 			</div>
 		);
