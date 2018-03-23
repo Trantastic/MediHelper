67df574a40c0fd24e9807b3186012abac671d2f5
Stacy Areas <stacyareas@gmail.com>
2018-03-22 21:27:31 -0700

    change class to className

--- client/src/components/PatientInputForm/PatientInputForm.js
+++ client/src/components/PatientInputForm/PatientInputForm.js
@@ -4,51 +4,51 @@ import "./PatientInputForm.css";
 const PatientInputForm = props => (
 	<div>
 		<form>
-  			<div class="form-group">
+  			<div className="form-group">
    				<label for="firstName">First Name</label>
-    			<input type="text" class="form-control" id="firstName">
+    			<input type="text" className="form-control" id="firstName">
     		</div>
-    		<div class="form-group">
+    		<div className="form-group">
    				<label for="lastName">Last Name</label>
-    			<input type="text" class="form-control" id="lastName">
+    			<input type="text" className="form-control" id="lastName">
     		</div>
-    		<div class="form-group">
+    		<div className="form-group">
     			<label for="address">Address</label>
-    			<input type="text" class="form-control" id="Address">
+    			<input type="text" className="form-control" id="Address">
   			</div>
-    		<div class="form-group">
+    		<div className="form-group">
    				<label for="primaryCare">Primary Care Physician</label>
-    			<input type="text" class="form-control" id="primaryCareName" placeholder="Physician Name">
-    			<input type="text" class="form-control" id="primaryCareNumb" placeholder="Physician's Phone Number">
+    			<input type="text" className="form-control" id="primaryCareName" placeholder="Physician Name">
+    			<input type="text" className="form-control" id="primaryCareNumb" placeholder="Physician's Phone Number">
     		</div>
-    		<div class="form-group">
+    		<div className="form-group">
    				<label for="emergencyContact">Emergency Contact</label>
-    			<input type="text" class="form-control" id="contactFirstName" placeholder="First Name">
-    			<input type="text" class="form-control" id="contactLastNAme" placeholder="Last Name">
-    			<input type="text" class="form-control" id="contactRelationship" placeholder="Relationship">
-    			<input type="text" class="form-control" id="contactNumb" placeholder="Phone Number">
+    			<input type="text" className="form-control" id="contactFirstName" placeholder="First Name">
+    			<input type="text" className="form-control" id="contactLastNAme" placeholder="Last Name">
+    			<input type="text" className="form-control" id="contactRelationship" placeholder="Relationship">
+    			<input type="text" className="form-control" id="contactNumb" placeholder="Phone Number">
     		</div>
-    		<div class="form-group">
+    		<div className="form-group">
    				<label for="language">Preferred Language</label>
-    			<input type="text" class="form-control" id="preferredLanguage">
+    			<input type="text" className="form-control" id="preferredLanguage">
     		</div>
-  			<div class="form-group">
+  			<div className="form-group">
     			<label for="medicalCond">Medical Condition(s)</label>
-    			<input type="text" class="form-control" id="medicalCond">
+    			<input type="text" className="form-control" id="medicalCond">
   			</div>
-  			<div class="form-group">
+  			<div className="form-group">
     			<label for="medication">Medication(s)</label>
-    			<input type="text" class="form-control" id="medication">
+    			<input type="text" className="form-control" id="medication">
   			</div>
-  			<div class="form-group">
+  			<div className="form-group">
     			<label for="allergies">Allergies</label>
-    			<input type="text" class="form-control" id="allergies" placeholder="Foods, Medications, Other">
+    			<input type="text" className="form-control" id="allergies" placeholder="Foods, Medications, Other">
   			</div>
-  			<div class="form-group">
+  			<div className="form-group">
     			<label for="notes">Notes</label>
-    			<input type="text" class="form-control" id="notes">
+    			<input type="text" className="form-control" id="notes">
   			</div>
-  			<button type="submit" class="btn btn-primary">Submit</button>
+  			<button type="submit" className="btn btn-primary">Submit</button>
 		</form>
 	</div>
 	);
