c45e26ebda046c526c5d7a9cfef532495be68dbf
kimmybeee <kimariehavertybozman@gmail.com>
2018-03-23 15:57:39 -0700

    patientView button mod and began psuedocode for twilio

--- client/src/components/PatientView/PatientView.js
+++ client/src/components/PatientView/PatientView.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./PatientView.css";
 
-function PatientView(props) {
-	return (
-		<button onClick={props.onClick}>
-			Help 
-		</button>	
-		);	
+//get caretaker phone number from db and assign a variable 
+
+//API.getPhone => access db=
+//setState {
+	//caretaker: caretaker info
+//}
+	//.then (res=> this.setState({
+//		caretaker phone: phone
+//		}))
+
+//caretakerPhone = ""
+
+
+
+getHelp = event => {
+
 }
+
+const PatientView = (props) =>(
+	<button onClick={() => props.getHelp}>
+		Help 
+	</button>
+);	
 
 
 export default PatientView;
