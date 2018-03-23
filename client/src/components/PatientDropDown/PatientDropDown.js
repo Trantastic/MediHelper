4ea635da9535dc06c4fdf849a2fbbcbd96016873
Yvette <yvettetran@gmail.com>
2018-03-23 08:57:39 -0700

    began code for patient drop down menu

---
+++ client/src/components/PatientDropDown/PatientDropDown.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PatientDropDown = props => {
+	<div className="dropdown">
+		<button onClick={props.getPatients} className="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			Dropdown button
+		</button>
+		<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+			<a className="dropdown-item" href="#">Add Patient</a>
+
+		</div>
+	</div>
+}
