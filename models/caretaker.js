677608d7ab87f305c6897456a17ad55d45d0262c
Maggie Falkenberg <mf1306@yahoo.com>
2018-03-26 11:09:54 -0700

    created models for patient and caretaker

--- models/caretaker.js
+++ models/caretaker.js
@@ -4,7 +4,13 @@ const Schema = mongoose.Schema;
 const caretakerSchema = new Schema({
 	username: {type: String, required: true},
 	password: {type: String, required: true},
+	phoneNum: {type: String, required: true}
 })
+
+patient: {
+	type: Schema.Types.ObjectId,
+	ref: "Patient"
+}
 
 const Caretaker = mongoose.model("Caretaker", caretakerSchema);
 
