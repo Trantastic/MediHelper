c5651102ca86e4a9e448da2c3bde1192e2e23cc9
kimmybeee <kimariehavertybozman@gmail.com>
2018-03-27 14:45:07 -0700

    added bootstrap modal to calendar

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -4,12 +4,13 @@ import ReactDOM from "react-dom";
 import FullCalendar from "fullcalendar-reactwrapper";
 import "./Calendar.css";
 
+
 const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
 
-let events = [];
+// let eventNew = [];
 
 
 
@@ -19,29 +20,73 @@ class CareCalendar extends React.Component {
 		this.state = {
 			event: [
 				{	
-					//Do I have to list every possible key? or Just required?
-					//Or maybe nothing and this is just an empty array???
-					title: "xxxxxxx",
-					start: "xxxxxxx",
-					description: "xxxxxxx"
-					// end: "xxxxxxx" (not a required field)
+					title: "",
+					start: ""
 				}
 			],
 		}
 	}
 
 	componentDidMount = () => {
-		db.Calendar.find({})
-		.then(function(dbCalendar)	{
-			res.json(dbCalendar.event);
-			events = dbCalendar.event;
+		db.Event.find({})
+		.then(function(dbEvent)	{
+			res.json(dbEvent.event);
 		})
 		.catch(function(err)	{
 			res.json(err);
 		});
-		console.log(events);
+		
 		this.setState({ event: events });
+		console.log(events);
 	};
+	//Save Changes button needs a click function that 1) grabs input fields 2)saves as an object 3) Sends onject to db to be stored as new event 
+	//4) renders new event on calendar
+	getEventInput = () => {
+		<div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">
+  			<div class="modal-dialog" role="document">
+			    <div class="modal-content">
+			      	<div class="modal-header">
+			        	<h5 class="modal-title" id="eventModalLabel">Create Event</h5>
+			        	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			         	 <span aria-hidden="true">&times;</span>
+			        	</button>
+			      	</div>
+			  	<div class="modal-body">
+				    <form>
+						<div class="form-group">
+					    	<label for="eventTitle">Event Name</label>
+					    	<input type="text" class="form-control" id="eventTitle" />
+						</div>
+						  	<div class="form-group">
+					    	<label for="eventStart">Event Start</label>
+					    	<input type="text" class="form-control" id="eventStart" placeholder=??? />
+					  	</div>
+					  	<div class="form-group">
+					    	<label for="eventEnd">Event End</label>
+					    	<input type="text" class="form-control" id="eventEnd" placeholder=??? />
+					  	</div>
+					  	<div class="form-group">
+					    	<label for="patientName">Regarding Patient</label>
+					    	<input type="text" class="form-control" id="patientName" placeholder="Patient Name" />
+					  	</div>
+					  	<div class="form-group">
+					    	<label for="eventDetails">Event Details</label>
+					    	<input type="text" class="form-control" id="eventDetails" />
+					  	</div>
+					</form>
+			    </div>
+			    <div class="modal-footer">
+			        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			        <button type="button" class="btn btn-primary">Save changes</button>
+			    </div>
+			</div>
+		</div>
+	}
+
+	makeEvent = () => {
+		getEventInput();
+		// db.Event.
+	}
 
 	render() {
 		return (
@@ -59,8 +104,7 @@ class CareCalendar extends React.Component {
 				customButtons= {
 					addEventButton: {
 						text: "Create A Calendar Event Here...",
-						//Do I define the function here or before and call it here???
-						click: ???
+						click: this.makeEvent
 					}
 				}
 				//Where do I add the fields for each event????
