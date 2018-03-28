1eeec113f5433364ab80c38e7eda62ec5648e90d
kimmybeee <kimariehavertybozman@gmail.com>
2018-03-28 13:47:44 -0700

    calendar work

--- client/src/components/Calendar/Calendar.js
+++ client/src/components/Calendar/Calendar.js
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import FullCalendar from "fullcalendar-reactwrapper";
 import "./Calendar.css";
-
+import "fullcalendar-reactwrapper/dist/css/fullcalendar.min.css";
+import db from "../../models";
 
 const mongoose = require("mongoose");
 const express = require("express");
@@ -27,95 +28,75 @@ class CareCalendar extends React.Component {
 		}
 	}
 
-	componentDidMount = () => {
-		db.Event.find({})
-		.then(function(dbEvent)	{
-			res.json(dbEvent.event);
-		})
-		.catch(function(err)	{
-			res.json(err);
-		});
+	componentDidMount() {
+		// db.Event.find({})
+		// .then(function(dbEvent)	{
+		// 	response.json(dbEvent.event);
+		// })
+		// .catch(function(err)	{
+		// 	response.json(err);
+		// });
 		
-		this.setState({ event: events });
-		console.log(events);
+		// this.setState({ event: events });
+		// console.log(events);
 	};
+
+
 	//Save Changes button needs a click function that 1) grabs input fields 2)saves as an object 3) Sends onject to db to be stored as new event 
 	//4) renders new event on calendar
-	getEventInput = () => {
-		<div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">
-  			<div class="modal-dialog" role="document">
-			    <div class="modal-content">
-			      	<div class="modal-header">
-			        	<h5 class="modal-title" id="eventModalLabel">Create Event</h5>
-			        	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-			         	 <span aria-hidden="true">&times;</span>
-			        	</button>
-			      	</div>
-			  	<div class="modal-body">
-				    <form>
-						<div class="form-group">
-					    	<label for="eventTitle">Event Name</label>
-					    	<input type="text" class="form-control" id="eventTitle" />
-						</div>
-						  	<div class="form-group">
-					    	<label for="eventStart">Event Start</label>
-					    	<input type="text" class="form-control" id="eventStart" placeholder=??? />
-					  	</div>
-					  	<div class="form-group">
-					    	<label for="eventEnd">Event End</label>
-					    	<input type="text" class="form-control" id="eventEnd" placeholder=??? />
-					  	</div>
-					  	<div class="form-group">
-					    	<label for="patientName">Regarding Patient</label>
-					    	<input type="text" class="form-control" id="patientName" placeholder="Patient Name" />
-					  	</div>
-					  	<div class="form-group">
-					    	<label for="eventDetails">Event Details</label>
-					    	<input type="text" class="form-control" id="eventDetails" />
-					  	</div>
-					</form>
-			    </div>
-			    <div class="modal-footer">
-			        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-			        <button type="button" class="btn btn-primary">Save changes</button>
-			    </div>
-			</div>
-		</div>
+	getEventInput() {
+		// <div className="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">
+  // 			<div className="modal-dialog" role="document">
+		// 	    <div className="modal-content">
+		// 	      	<div className="modal-header">
+		// 	        	<h5 className="modal-title" id="eventModalLabel">Create Event</h5>
+		// 	        	<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+		// 	         	 <span aria-hidden="true">&times;</span>
+		// 	        	</button>
+		// 	      	</div>
+		// 	  	<div className="modal-body">
+		// 		    <form>
+		// 				<div className="form-group">
+		// 			    	<label for="eventTitle">Event Name</label>
+		// 			    	<input type="text" className="form-control" id="eventTitle" />
+		// 				</div>
+		// 				  	<div className="form-group">
+		// 			    	<label for="eventStart">Event Start</label>
+		// 			    	<input type="text" className="form-control" id="eventStart" />
+		// 			  	</div>
+		// 			  	<div className="form-group">
+		// 			    	<label for="eventEnd">Event End</label>
+		// 			    	<input type="text" className="form-control" id="eventEnd" placeholder="name me" />
+		// 			  	</div>
+		// 			  	<div className="form-group">
+		// 			    	<label for="patientName">Regarding Patient</label>
+		// 			    	<input type="text" className="form-control" id="patientName" placeholder="Patient Name" />
+		// 			  	</div>
+		// 			  	<div className="form-group">
+		// 			    	<label for="eventDetails">Event Details</label>
+		// 			    	<input type="text" className="form-control" id="eventDetails" />
+		// 			  	</div>
+		// 			</form>
+		// 	    </div>
+		// 	    <div className="modal-footer">
+		// 	        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+		// 	        <button type="button" className="btn btn-primary">Save changes</button>
+		// 	    </div>
+		// 	</div>
+		// </div>
 	}
 
-	makeEvent = () => {
-		getEventInput();
+	makeEvent() {
+		// getEventInput()
 		// db.Event.
-	}
+	};
 
 	render() {
 		return (
 			<div id="care-calendar">
-				<FullCalendar 
-					id = "xxxxxxx"
-				header = {{
-					left: "prev,next",
-					center: "title",
-					right: "month,agendaWeek,agendaDay,listWeek"
-				}}
-				footer = {{
-					left: addEventButton
-				}}
-				customButtons= {
-					addEventButton: {
-						text: "Create A Calendar Event Here...",
-						click: this.makeEvent
-					}
-				}
-				//Where do I add the fields for each event????
-				defaultDate={ moment() }
-				navLinks= { true }
-				editable= { true }
-				eventLimit= { true }
-				events = { this.state.event }
-				/>
+				
 			</div>
-
+		
 		);
 	}
 }
--- client/src/components/Help/Help.js
+++ client/src/components/Help/Help.js
@@ -16,23 +16,23 @@ class Help extends React.Component {
 		helpNumber: ""
 	};
 
-	componentDidMount = () => {
-		//HOW DO I ACCESS THE USER HERRRRREEEEE???
-		db.Caretaker.find({ req.user.username })
-		.then(function(dbCaretaker)	{
-			res.json(dbCaretaker.phoneNum);
-			caretakerPhone = dbCaretaker.phoneNum;
-		})
-		.catch(function(err)	{
-			res.json(err);
-		});
-		//Add "+1" to the beginning of the res.Caretaker.phoneNum
-		caretakerPhone = "+1" + caretakerPhone;
-		console.log(caretakerPhone);
-		this.setState({ helpNumber: caretakerPhone });
-		});
-		//The above ) mat be an uneccessary token
-	};
+	// componentDidMount = () => {
+	// 	//HOW DO I ACCESS THE USER HERRRRREEEEE???
+	// 	db.Caretaker.find({ req.user.username })
+	// 	.then(function(dbCaretaker)	{
+	// 		res.json(dbCaretaker.phoneNum);
+	// 		caretakerPhone = dbCaretaker.phoneNum;
+	// 	})
+	// 	.catch(function(err)	{
+	// 		res.json(err);
+	// 	});
+	// 	//Add "+1" to the beginning of the res.Caretaker.phoneNum
+	// 	caretakerPhone = "+1" + caretakerPhone;
+	// 	console.log(caretakerPhone);
+	// 	this.setState({ helpNumber: caretakerPhone });
+	// 	});
+	// 	//The above ) mat be an uneccessary token
+	// };
 
 	getHelp = () => {
 		
