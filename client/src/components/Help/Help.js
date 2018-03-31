3f66fe218c64636f401f8c1aaf532bbc8c61c47d
kimmybeee <kimariehavertybozman@gmail.com>
2018-03-31 14:34:17 -0700

    small resollution missed in last merge

--- client/src/components/Help/Help.js
+++ client/src/components/Help/Help.js
@@ -48,11 +48,7 @@ class Help extends React.Component {
 		return (
 			<div className="help-button-div">
 			{this.asyncSolver()}
-<<<<<<< HEAD
-				<h1>Home Page</h1>
-=======
 				<h3>If you need assistance, press help to notify your caretaker</h3>
->>>>>>> 75d65748277ad451563ec2c838f284d85d12df27
 				<button type="button" className="help-button btn-danger btn-block" onClick={ this.getHelp }>Help</button>
 			</div>
 		);
