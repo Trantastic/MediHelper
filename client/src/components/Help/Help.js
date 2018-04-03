1d35589917ac2dd7da2d988cada815829feda648
Stacy Areas <stacyareas@gmail.com>
2018-04-02 18:51:07 -0700

    add image logo -- add image help button

--- client/src/components/Help/Help.js
+++ client/src/components/Help/Help.js
@@ -1,62 +1,3 @@
-// import React from "react";
-// const mongoose = require("mongoose");
-// const express = require("express");
-
-// const app = express();
-
-// //Variable to hold the phone number texted for assistence
-// let caretakerPhone = "";
-
-// // Twilio Credentials
-// const accountSid = "ACf6c36a2408031fa42185a595059cd3fa";
-// const authToken = "42e61773eba63f88e812a370013294aa";
-
-// class Help extends React.Component {
-// 	state = {
-// 		helpNumber: ""
-// 	};
-
-// 	// componentDidMount = () => {
-// 	// 	//HOW DO I ACCESS THE USER HERRRRREEEEE???
-// 	// 	db.Caretaker.find({ req.user.username })
-// 	// 	.then(function(dbCaretaker)	{
-// 	// 		res.json(dbCaretaker.phoneNum);
-// 	// 		caretakerPhone = dbCaretaker.phoneNum;
-// 	// 	})
-// 	// 	.catch(function(err)	{
-// 	// 		res.json(err);
-// 	// 	});
-// 	// 	//Add "+1" to the beginning of the res.Caretaker.phoneNum
-// 	// 	caretakerPhone = "+1" + caretakerPhone;
-// 	// 	console.log(caretakerPhone);
-// 	// 	this.setState({ helpNumber: caretakerPhone });
-// 	// 	});
-// 	// };
-
-// 	getHelp = () => {
-		
-
-// 		// require the Twilio module and create a REST client
-// 		const client = require('twilio')(accountSid, authToken);
-
-// 		client.messages
-// 		  .create({
-// 		    to: this.state.helpNumber,
-// 		    from: "+18312469287",
-// 		    body: "Your patient needs help! Please assist!",
-// 		  })
-// 		 .then((message) => console.log(message.sid));
-// 	};
-// 	render() {
-// 		return (
-// 			<div className="help-button-div">
-// 				<h1>Home Page</h1>
-// 				<button type="button" className="help-button btn-danger btn-block" onClick={ this.getHelp }>Help</button>
-// 			</div>
-// 		);
-// 	}
-// }
-// export default Help;
 import React from 'react';
 import 'whatwg-fetch';
 import "./Help.css";
@@ -79,7 +20,6 @@ class Help extends React.Component {
 
 	componentDidMount = () => {
 		console.log("4 This is happening");
-		// helpNumber = "+1" + this.props.caretaker.phoneNumb;
 		console.log("5" + helpNumber);
 	};
 
@@ -108,8 +48,10 @@ class Help extends React.Component {
 		return (
 			<div className="help-button-div container">
 			{this.asyncSolver()}
-				<h3 className="helpHeader text-center mt-5">If you need assistance, press help to notify your caretaker by clicking the button</h3>
-				<button type="button" className="help-button btn-danger btn-block" onClick={ this.getHelp }>Help</button>
+				<input type="image" className="help-button img-fluid" src="https://cdn1.iconfinder.com/data/icons/medical-services-set-2/256/10-512.png" onClick={ this.getHelp } />
+				<h3 className="helpHeader text1 text-center">If you need</h3>
+				<h3 className="helpHeader text2 text-center">assistance</h3>
+				<h3 className="helpHeader text3 text-center">click here</h3>
 			</div>
 		);
 	}
